a051ef1cb03360ab1c7864f49cf6fddffdea017f
emanuel36 <emanuel.tiec@gmail.com>
2019-09-24 14:22:38 -0300

    sht30 driver error detection

--- inc/Peripheral Drivers/sht30.h
+++ inc/Peripheral Drivers/sht30.h
@@ -1,10 +1,12 @@
 #ifndef SHT30_H
 #define	SHT30_H
 
+#include <stdbool.h>
+
 #define SHT30_ADRESS 0x88
 #define COMMAND_BYTE_1 0x24
 #define COMMAND_BYTE_2 0x16
 
-void SHT30Read(float *temp, float *humi);
+bool SHT30Read(float *temp, float *humi);
 
 #endif	/* SHT30_H */
--- src/Peripheral Drivers/sht30.c
+++ src/Peripheral Drivers/sht30.c
@@ -1,24 +1,26 @@
 #include "../../inc/Peripheral Drivers/sht30.h"
 #include "../../inc/MCC Drivers/i2c.h"
 
-void SHT30Read(float *temp, float *humi){
+bool SHT30Read(float *temp, float *humi){
     unsigned int dataT = 0, dataRH = 0;
     char checksumTemp = 0, checksumHumi = 0;
     
-    I2C_Start(SHT30_ADRESS);
-    I2C_Write(COMMAND_BYTE_1);
-    I2C_Write(COMMAND_BYTE_2);
-    I2C_Stop();
+    if(!(I2C_Start(SHT30_ADRESS)))      return false;
+    if(!(I2C_Write(COMMAND_BYTE_1)))    return false;
+    if(!(I2C_Write(COMMAND_BYTE_2)))    return false;
+    if(!(I2C_Stop()))                   return false;
     
-    I2C_Start(SHT30_ADRESS + 1);
+    if(!(I2C_Start(SHT30_ADRESS + 1)))  return false;
     dataT = I2C_Read(ACK) << 8;
     dataT |= I2C_Read(ACK);
     checksumTemp = I2C_Read(0);
     dataRH = I2C_Read(ACK) << 8;
     dataRH |= I2C_Read(ACK);
     checksumHumi = I2C_Read(NACK);
-    I2C_Stop();       
+    if(!(I2C_Stop()))                   return false;      
         
     *temp = (( (float) dataT * 175.0) / 65535.0) - 45.0;
     *humi = ( (float) dataRH * 100.0) / 65535.0;  
+    
+    return true;
 }
